3e44d4c9901fd7ab2ca89034f360f611da86318f
DJHyun <djestiny4444@naver.com>
2019-05-13 07:29:48 +0000

    dj commit

--- BOB/momo/models.py
+++ BOB/momo/models.py
@@ -5,11 +5,11 @@ from django.conf import settings
 # Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=120) # 영화 제목
-    poster_url = models.ImageField(blank=False) # 영화 포스터
+    poster_url = models.ImageField(blank=True) # 영화 포스터
     audiCnt = models.IntegerField() # 해당일 관객수
     audinten = models.IntegerField() # 전일 대비 증감수
     audiChange = models.IntegerField() # 전일 대비 증감 비율
-    auidAcc = models.IntegerField() # 누적 관객수
+    audiAcc = models.IntegerField() # 누적 관객수
     userRating = models.IntegerField() # 영화 평점
     
 class Match(models.Model):
@@ -19,4 +19,4 @@ class Match(models.Model):
     user_up = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="up",blank=True)
     user_down = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="down",blank=True)
     standard = models.PositiveIntegerField(default = 0)
-    date = models.DateField()
+    date = models.DateField(auto_now_add=True)
